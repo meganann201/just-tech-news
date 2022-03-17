566775154143bb66194a8b78e6fd6a921d2a8df1
Megan Campbell <megancampbell201@gmail.com>
2022-03-17 13:28:02 -0400

    heroku db not working

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,9 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-
-require('dotenv').config();
+// require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+sequelize = new Sequelize(process.env.JAWSDB_URL);
   
-  module.exports = sequelize;
+module.exports = sequelize;
