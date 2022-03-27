f3d7211e863d6c053e42b2c3eb2bf637eab03f7f
Megan Campbell <megancampbell201@gmail.com>
2022-03-27 11:53:53 -0400

    created edit page and functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+
+
 router.get('/', (req, res) => {
   Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
